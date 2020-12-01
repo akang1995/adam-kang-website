b837421d965985d952d8016a7de355d0fd34e878
akang1995 <adamkang95@gmail.com>
2020-11-30 23:19:59 -0800

    first commit

---
+++ src/containers/About.js
@@ -0,0 +1,171 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Grid from "@material-ui/core/Grid";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+
+import headshot from "../assets/headshot.jpg";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: 20,
+    marginRight: "15%",
+    marginLeft: "15%"
+  },
+  image: {
+    maxWidth: "14%",
+    borderRadius: 200
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  },
+  textBlock: {
+    marginBottom: "2%"
+  },
+  projectBlock: {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: "2%"
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 600
+  },
+  header: {
+    fontSize: 42,
+    fontWeight: 600,
+    marginTop: "1%",
+    marginBottom: "3%"
+  }
+}));
+
+const SocialData = [
+  { icon: <TwitterIcon />, url: "https://twitter.com/AdamKang18" },
+  { icon: <EmailIcon />, url: "mailto:adamkang95@gmail.com" },
+  { icon: <GitHubIcon />, url: "https://github.com/akang1995" },
+  { icon: <LinkedInIcon />, url: "https://www.linkedin.com/in/adamkang/" }
+];
+
+const TextData = [
+  {
+    text:
+      "👋🏼 hey there, my name is Adam and I'm the co-founder of biteschool - a mobile app that lets you learn valuable skills and concepts on the go, from your phone."
+  },
+  {
+    text:
+      "👨🏻‍💻 previously, I was an early employee at Darkstore (software dev + strategy & finance) and spent 3 years prior to that in investment banking at J.P. Morgan advising software and internet companies on M&A and capital raising."
+  },
+  {
+    text:
+      "🔥 self taught programmer. I'm passionate about continuous education and don't believe there will be a day where we won't need to learn anything new."
+  },
+  {
+    text:
+      "💯 outside of biteschool, I love shipping micro saas ideas, dabbling in e-commerce, reading about tech, managing my equities portfolio, improving my short game and watching soccer."
+  },
+  {
+    text: (
+      <p>
+        🗯 let's connect! I'd love to chat
+        <text style={{ color: "red" }}> micro saas</text>,
+        <text style={{ color: "red" }}> startup strategy</text>,
+        <text style={{ color: "red" }}> edtech</text>,
+        <text style={{ color: "red" }}> react/react native</text> or sports.
+      </p>
+    )
+  },
+  {
+    text: "🇨🇦 ubc grad, spending time between vancouver & sf."
+  }
+];
+
+const ProjectData = [
+  {
+    name: "biteschool",
+    url:
+      "https://apps.apple.com/app/id1526501493?fbclid=IwAR2LxUdyAoroZ_tJVejTcEpDCz62mShfZMnM1GKYlpvfQ4_xYQy9eKO_uNg",
+    description:
+      " a mobile learning app that teaches relevant skills through bite-sized lessons"
+  }
+];
+
+// const headshot = require("../assets/headshot.jpg");
+
+export const About = () => {
+  const classes = useStyles();
+  const TextBlock = ({ text }) => (
+    <div className={classes.textBlock}>{text}</div>
+  );
+
+  const ProjectBlock = ({ name, url, description }) => (
+    <div className={classes.projectBlock}>
+      <a
+        href={url}
+        style={{
+          cursor: "pointer",
+          color: "red",
+          fontWeight: 600,
+          textDecoration: "none"
+        }}
+      >
+        {name}
+      </a>
+      :{description}
+    </div>
+  );
+
+  const HeaderText = ({ text }) => (
+    <Grid item xs={12}>
+      <div className={classes.header}>{text}</div>
+    </Grid>
+  );
+
+  const Title = ({ text }) => (
+    <Grid item xs={12}>
+      <div className={classes.title}>{text}</div>
+    </Grid>
+  );
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <HeaderText text="adam kang" />
+        <Grid container>
+          <img src={headshot} alt="Italian Trulli" className={classes.image} />
+        </Grid>
+        <Grid
+          container
+          xs={2}
+          style={{ padding: "1%", marginTop: "2%" }}
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        >
+          {SocialData.map(({ icon, url }) => (
+            <a href={url} style={{ cursor: "pointer", color: "#4a4a4a" }}>
+              {icon}
+            </a>
+          ))}
+        </Grid>
+        <Title text="about" />
+        <Grid item xs={12}>
+          {TextData.map(({ text }) => (
+            <TextBlock text={text} />
+          ))}
+        </Grid>
+        <Title text="projects" />
+        <Grid item xs={12}>
+          {ProjectData.map(({ name, url, description }) => (
+            <ProjectBlock name={name} url={url} description={description} />
+          ))}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
